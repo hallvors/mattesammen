60d39fc05f3750590d1052ed58958e5a60677cca
Hallvord R. M. Steen <hallvord@minus.no>
2025-08-26 22:43:14 +0200

    Fix bugs in geo bingo rendering

--- client/js/geobingo.js
+++ client/js/geobingo.js
@@ -8,8 +8,8 @@ var selectedAlready = false;
 var level = 1;
 
 var cards = [];
-for(var i = 0; i < 25; i++) {
-  var item = getRandomWholeNumber(0, keys.length - 1);
+for (var i = 0; i < 25; i++) {
+  var item = getRandomWholeNumber(0, keys.length);
   cards.push({
     type: keys[item],
     selected: false,
@@ -24,45 +24,49 @@ function init() {
 }
 
 function evtHandler(itemnr) {
-  return function() {
+  return function () {
     if (selectedAlready) {
       return;
     }
     if (cards[itemnr].type === theAnswer && !cards[itemnr].selected) {
       cards[itemnr].selected = true;
-      cards[itemnr].cell.attr('stroke', '#090')
-      cards[itemnr].cell.attr('fill', '#efe')
+      cards[itemnr].cell.attr('stroke', '#090');
+      cards[itemnr].cell.attr('fill', '#efe');
       selectedAlready = true;
-      console.log('will emit answer', {level: level, cards: cards})
-      socket.emit('correct-answer', {level: level, cards: cards.map(
-        card => ({type: card.type, selected: card.selected})
-      )});
+      console.log('will emit answer', { level: level, cards: cards });
+      socket.emit('correct-answer', {
+        level: level,
+        cards: cards.map((card) => ({
+          type: card.type,
+          selected: card.selected,
+        })),
+      });
       // BINGO?
-      for(var i = 0; i < 5; i++) {
+      for (var i = 0; i < 5; i++) {
         if (itemnr >= i * 5 && itemnr < (i + 1) * 5) {
-          var state = cards[i * 5].selected &&
-            cards[(i * 5) + 1].selected &&
-            cards[(i * 5) + 2].selected &&
-            cards[(i * 5) + 3].selected &&
-            cards[(i * 5) + 4].selected;
+          var state =
+            cards[i * 5].selected &&
+            cards[i * 5 + 1].selected &&
+            cards[i * 5 + 2].selected &&
+            cards[i * 5 + 3].selected &&
+            cards[i * 5 + 4].selected;
           if (state) {
             level++;
             bounceStars();
-            socket.emit('bingo', {level: level, classId: classId});
+            socket.emit('bingo', { level: level, classId: classId });
           }
         }
       }
     }
-  }
+  };
 }
 
-socket.on('new-bingo-answer', function(payload) {
-  console.log('new-bingo-answer', payload)
+socket.on('new-bingo-answer', function (payload) {
+  console.log('new-bingo-answer', payload);
   if (payload.answer) {
     theAnswer = payload.answer;
     selectedAlready = false;
   }
 });
-
 
 window.onload = init;
