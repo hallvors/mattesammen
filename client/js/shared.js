f65762d0c530d625a1601b2fd4aff828712b729e
Hallvord R. M. Steen <hallvord@minus.no>
2025-08-26 22:52:46 +0200

    Some prettier, some geobingo shape bug fix

--- client/js/shared.js
+++ client/js/shared.js
@@ -4,7 +4,7 @@ Functions used on both admin- and pupil-screens
 function elm(tag, props, children, parent) {
   var el = document.createElement(tag);
   for (var p in props) {
-    if (typeof props[p] === "function") {
+    if (typeof props[p] === 'function') {
       el[p] = props[p];
     } else {
       el.setAttribute(p, props[p]);
@@ -20,7 +20,6 @@ function elm(tag, props, children, parent) {
   }
   return el;
 }
-
 
 function handleConnect() {
   console.log(arguments);
@@ -38,14 +37,16 @@ function handleReconnect() {
   console.log(arguments);
 }
 
-socket.on("connect", handleConnect);
-socket.on("error", handleError);
-socket.on("disconnect", handleDisconnect);
-socket.on("reconnect", handleReconnect);
+socket.on('connect', handleConnect);
+socket.on('error', handleError);
+socket.on('disconnect', handleDisconnect);
+socket.on('reconnect', handleReconnect);
 
 function bounceStars() {
-  document.getElementById("stars").className = "bounce";
-  setTimeout(function(){document.getElementById("stars").className = "";}, 800);
+  document.getElementById('stars').className = 'bounce';
+  setTimeout(function () {
+    document.getElementById('stars').className = '';
+  }, 800);
 }
 
 function getRandomWholeNumber(min, max) {
@@ -56,75 +57,94 @@ var figureCode = {
   circle: function (elm, canvWidth) {
     return elm.circle(canvWidth / 2, canvWidth / 2, canvWidth / 4);
   },
-  square: function(elm, canvWidth) {
+  square: function (elm, canvWidth) {
     return elm.rect(canvWidth / 3, canvWidth / 3, canvWidth / 3, canvWidth / 3);
   },
-  rectangle: function(elm, canvWidth) {
-    return elm.rect(canvWidth / 5, canvWidth / 2.5, canvWidth / 5 * 3, canvWidth / 5);
+  rectangle: function (elm, canvWidth) {
+    return elm.rect(
+      canvWidth / 5,
+      canvWidth / 2.5,
+      (canvWidth / 5) * 3,
+      canvWidth / 5
+    );
   },
-  triangle1: function(elm, canvWidth) {
+  triangle1: function (elm, canvWidth) {
     var path = [
-      'M' + (canvWidth / 2) + ',' + (canvWidth / 3),
-      'l' + (canvWidth / 3) + ',' + (canvWidth / 3),
-      'l-' + (canvWidth - (canvWidth / 3)) + ',' + 0,
-      'L' + (canvWidth / 2) + ',' + (canvWidth / 3),
+      'M' + canvWidth / 2 + ',' + canvWidth / 3,
+      'l' + canvWidth / 3 + ',' + canvWidth / 3,
+      'l-' + (canvWidth - canvWidth / 3) + ',' + 0,
+      'L' + canvWidth / 2 + ',' + canvWidth / 3,
     ];
     return elm.path(path.join(''));
   },
-  triangle2: function(elm, canvWidth) {
+  triangle2: function (elm, canvWidth) {
     var path = [
-      'M' + (canvWidth / 3) + ',' + (canvWidth / 3),
-      'l' + (canvWidth / 3) + ',' + (canvWidth / 3),
-      'l-' + (canvWidth / 3) + ',' + 0,
-      'L' + (canvWidth / 3) + ',' + (canvWidth / 3),
+      'M' + canvWidth / 3 + ',' + canvWidth / 3,
+      'l' + canvWidth / 3 + ',' + canvWidth / 3,
+      'l-' + canvWidth / 3 + ',' + 0,
+      'L' + canvWidth / 3 + ',' + canvWidth / 3,
     ];
     return elm.path(path.join(''));
   },
-  poly5: function(elm, canvWidth) {
-    var x = canvWidth / 2, y = canvWidth / 2, r = canvWidth / 4, n = 5;
-    var xx, yy, i, a, pathString = '';
+  poly5: function (elm, canvWidth) {
+    var x = canvWidth / 2,
+      y = canvWidth / 2,
+      r = canvWidth / 4,
+      n = 5;
+    var xx,
+      yy,
+      i,
+      a,
+      pathString = '';
     for (i = 0; i <= n; ++i) {
-        a = ((4 * Math.PI * i) + (Math.PI * n) + (2 * Math.PI)) / (2 * n);
-        xx = x + r * Math.cos(a);
-        yy = y + r * Math.sin(a);
-        pathString += (i == 0 ? "M " : " L ") + xx + " " + yy;
+      a = (4 * Math.PI * i + Math.PI * n + 2 * Math.PI) / (2 * n);
+      xx = x + r * Math.cos(a);
+      yy = y + r * Math.sin(a);
+      pathString += (i == 0 ? 'M ' : ' L ') + xx + ' ' + yy;
     }
-    pathString += " z";
+    pathString += ' z';
     return elm.path(pathString);
   },
-  poly6: function(elm, canvWidth) {
-    var x = canvWidth / 2, y = canvWidth / 2, r = canvWidth / 4, n = 6;
-    var xx, yy, i, a, pathString = '';
+  poly6: function (elm, canvWidth) {
+    var x = canvWidth / 2,
+      y = canvWidth / 2,
+      r = canvWidth / 4,
+      n = 6;
+    var xx,
+      yy,
+      i,
+      a,
+      pathString = '';
     for (i = 0; i <= n; ++i) {
-        a = ((4 * Math.PI * i) + (Math.PI * n) + (2 * Math.PI)) / (2 * n);
-        xx = x + r * Math.cos(a);
-        yy = y + r * Math.sin(a);
-        pathString += (i == 0 ? "M " : " L ") + xx + " " + yy;
+      a = (4 * Math.PI * i + Math.PI * n + 2 * Math.PI) / (2 * n);
+      xx = x + r * Math.cos(a);
+      yy = y + r * Math.sin(a);
+      pathString += (i == 0 ? 'M ' : ' L ') + xx + ' ' + yy;
     }
-    pathString += " z";
+    pathString += ' z';
     return elm.path(pathString);
   },
-  paralellogram: function(elm, canvWidth) {
+  paralellogram: function (elm, canvWidth) {
     var path = [
-      'M' + (canvWidth / 2) + ',' + (canvWidth / 3),
-      'l' + (canvWidth / 3) + ', 0',
-      'l' + (canvWidth / 3) + ',' + (canvWidth / 3),
-      'l-' + (canvWidth / 3) + ',' + 0,
-      'L' + (canvWidth / 2) + ',' + (canvWidth / 3),
+      'M' + canvWidth / 6 + ',' + canvWidth / 3,
+      'l' + canvWidth / 3 + ', 0',
+      'l' + canvWidth / 3 + ',' + canvWidth / 3,
+      'l-' + canvWidth / 3 + ',' + 0,
+      'Z',
     ];
     return elm.path(path.join(''));
   },
-  trapes: function(elm, canvWidth) {
+  trapes: function (elm, canvWidth) {
     var path = [
-      'M' + (canvWidth / 2) + ',' + (canvWidth / 3),
-      'l' + (canvWidth / 3) + ', 0',
-      'l' + (canvWidth / 3) + ',' + (canvWidth / 3),
-      'l-' + ((canvWidth / 3) * 2) + ',' + 0,
-      'L' + (canvWidth / 2) + ',' + (canvWidth / 3),
+      'M' + canvWidth / 3 + ',' + canvWidth / 3,
+      'l' + canvWidth / 3 + ', 0',
+      'l' + canvWidth / 4 + ',' + canvWidth / 4,
+      'l-' + (canvWidth / 12) * 10 + ',0',
+      'Z',
     ];
+    console.log(path);
     return elm.path(path.join(''));
   },
-
 };
 
 var keys = Object.keys(figureCode);
@@ -146,13 +166,13 @@ function createAndFillGrid(parentElm, cards, evtHandler) {
       cards[counter].elm = elm;
       cards[counter].cell = border;
       if (cards[counter].selected) {
-      	border.attr('stroke', '#090');
-      	border.attr('fill', '#efe');
+        border.attr('stroke', '#090');
+        border.attr('fill', '#efe');
       }
       if (evtHandler) {
-      	border.click(evtHandler(counter));
+        border.click(evtHandler(counter));
       }
-      counter ++;
+      counter++;
     }
   }
 }
